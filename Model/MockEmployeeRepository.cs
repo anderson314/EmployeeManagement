f3c216b05a57c6cdb3604aeb08ad2754964575cc
Anderson Rodrigo <andersonrodrigo03.14@gmail.com>
2021-10-27 22:23:02 -0300

    Add project files.

---
+++ Model/MockEmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Model
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@gmail.com"},
+                new Employee() { Id = 2, Name = "Jhon", Department = "IT", Email = "jhon@gmail.com"},
+                new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@gmail.com"},
+
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;   
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using EmployeeManagement.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            //'MvcOptions.EnableEndpointRouting = false' inside
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // It Costumizes UseDefaultFiles to accept another html file as default
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            // It clears the current default file
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            // It adds the new default file
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            //app.UseFileServer();
+
+            //app.UseDefaultFiles(defaultFilesOptions);
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name:"default", 
+                    pattern: "{controller=Home}/{action=Index}"
+                    );
+
+                //endpoints.MapControllers();
+
+                /*endpoints.MapGet("", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World: " + env.EnvironmentName);
+                });*/
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World: " + env.EnvironmentName);
+            });
+
+        }
+    }
+}
